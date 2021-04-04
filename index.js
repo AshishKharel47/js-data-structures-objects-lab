653edeb7bb1374c33eb696fbd06d44ec9cc042fc
AshishKharel <ashishkharel58@gmail.com>
2021-04-04 10:38:49 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const driver ={name:'Ashish kharel', street: '8060 ashland Ave'}
+function updateDriverWithKeyAndValue(obj,key,value){
+    const nextDriver={...obj};
+    nextDriver[key]= value;
+    return nextDriver;
+}
+function destructivelyUpdateDriverWithKeyAndValue(obj,key,value){
+    obj[key]=value;
+    return obj;
+}
+function deleteFromDriverByKey(obj,key){
+    const driver3={...obj};
+    delete driver3[key];
+    return driver3;
+}
+function destructivelyDeleteFromDriverByKey(obj,key){
+    delete obj[key];
+    return obj;
+}
